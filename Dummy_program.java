b94748d9e7fb03900ac0802f85bc638a0b268b70
rk.test <rk.test@gmail.com>
2016-05-27 19:47:24 -0500

    first commit

---
+++ Dummy_program.java
@@ -0,0 +1,13 @@
+package com.dummy_1;
+
+public class Dummy_program {
+
+	public static void main(String[] args) {
+		
+		
+			
+		
+
+	}
+
+}
---
+++ method1.java
@@ -0,0 +1,8 @@
+package com.dummy_1;
+
+public class method1 {
+
+	public void multiple1(){
+		System.out.println("printing 1");
+	}
+}
